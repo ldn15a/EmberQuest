2a6894ed6ff5ada6195c1d8dfe603b6faff1cf82
Lucas Newton <43220419+LucasNewton@users.noreply.github.com>
2019-02-18 17:27:14 -0600

    Defaults to Firefox instead of Chrome
    Chrome will give permission errors to access other files on local system. Shouldn't be an issue when pushed to a "real" server

--- launcher.bat
+++ launcher.bat
@@ -1,11 +1,6 @@
 SET pixiProjectPath=%~dp0
 
-:: Open pixi application the prefered way
-start chrome --new-window "http://127.0.0.1:8080"
-
-::uncomment the next line and comment out the previous line if chrome isn't working due to permissions
-::start firefox --new-window "http://127.0.0.1:8080"
-
-::If you are still having trouble then you can change the address from "http://192.168.0.2:8080" to whatever ip is printed in command prompt
+::  We have to use firefox because Chrome will throw permission errors which we can look into later. This is a security feature of Chrome
+start firefox --new-window "http://127.0.0.1:8080"
 
 http-server %pixiProjectPath:%
