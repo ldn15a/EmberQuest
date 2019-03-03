7270c492d087253a64e1bf2fb17932041a0ef963
Lucas Newton <ldn15a@acu.edu>
2019-03-02 20:28:41 -0600

    Did changes and pushing to merge with Troy

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,4 @@
-http-server &
+cd ~/Documents/ACU\ Junior/Spring\ 2019/DET/Ember\ Quest/
+php -S localhost:8000 -t ./ &
 cd /
-./Applications/Firefox.app/Contents/MacOS/firefox  --new-window 127.0.0.1:8080
+./Applications/Firefox.app/Contents/MacOS/firefox  --new-window localhost:8000
